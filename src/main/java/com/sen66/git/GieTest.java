7c39cf768a67984b8f46b643a76531f0ed4d2eac
Sen66 <csf980215@gmail.com>
2022-11-28 21:51:28 +0800

    first commit

---
+++ src/main/java/com/sen66/git/GieTest.java
@@ -0,0 +1,11 @@
+package com.sen66.git;
+
+/**
+ * @author sen66
+ * @date 2022/11/28 21:49
+ */
+public class GieTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
