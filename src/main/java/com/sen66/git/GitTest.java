27e912a9869e561ac81b88dfd98ab0b379e085fe
Sen66 <csf980215@gmail.com>
2022-11-28 22:04:48 +0800

    hot-fix commit2

--- src/main/java/com/sen66/git/GitTest.java
+++ src/main/java/com/sen66/git/GitTest.java
@@ -4,7 +4,7 @@ package com.sen66.git;
  * @author sen66
  * @date 2022/11/28 21:49
  */
-public class GieTest {
+public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
